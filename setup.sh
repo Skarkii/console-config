3d177a2cc103a4f8c74a421ebee9eb7c0a8942ed
skarkii <lucas@kuja-halkola.com>
2025-02-06 17:05:49 +0100

    multi cd..

--- setup.sh
+++ setup.sh
@@ -57,6 +57,11 @@ echo "
 
 # cd commands
 alias ..='cd ..'
+alias 1..='cd ..'
+alias 2..='cd ../..'
+alias 3..='cd ../../..'
+alias 4..='cd ../../../..'
+alias 5..='cd ../../../../..'
 
 # ls commands
 alias l='ls --color=auto'
